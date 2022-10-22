22fc680b0565be94292f947464080eb1f4d5138f
Vladimir_Pestov <Vladimir.Pestov@urfu.me>
2022-10-22 17:22:05 +0500

    Added base game manager class

--- game.py
+++ game.py
@@ -12,6 +12,8 @@ class Game(metaclass=Singleton):
                  back_image_filename,
                  frame_rate):
         self.background_image = pygame.image.load(back_image_filename)
+        self.background_image = pygame.transform.scale(self.background_image,
+                                                       (width, height))
         self.frame_rate = frame_rate
         self.__init_pygame_components()
         self.surface = pygame.display.set_mode((width, height))
@@ -19,24 +21,23 @@ class Game(metaclass=Singleton):
         self.clock = pygame.time.Clock()
         self.__init_key_handlers()
 
-    def __init_key_handlers(self):
-        self.keydown_handlers = defaultdict(list)
-        self.keyup_handlers = defaultdict(list)
-        self.mouse_handlers = []
+    def run(self):
+        while not self.game_over:
+            self.surface.blit(self.background_image, (0, 0))
 
-    def __init_pygame_components(self):
-        self.game_over = False
-        self.objects = []
-        pygame.mixer.pre_init(44100, 16, 2, 4096)
-        pygame.init()
-        pygame.font.init()
+            self.handle_events()
+            self.update()
+            self.draw()
+
+            pygame.display.update()
+            self.clock.tick(self.frame_rate)
 
     def update(self):
-        for game_object in self.objects:
+        for game_object in self.__objects:
             game_object.update()
 
     def draw(self):
-        for game_object in self.objects:
+        for game_object in self.__objects:
             game_object.draw(self.surface)
 
     def handle_events(self):
@@ -52,13 +53,25 @@ class Game(metaclass=Singleton):
                 for handler in self.mouse_handlers:
                     handler(event.type, event.pos)
 
+    def __init_key_handlers(self):
+        self.keydown_handlers = defaultdict(list)
+        self.keyup_handlers = defaultdict(list)
+        self.mouse_handlers = []
+
+    def __init_pygame_components(self):
+        self.game_over = False
+        self.__objects = []
+        pygame.mixer.pre_init(44100, 16, 2, 4096)
+        pygame.init()
+        pygame.font.init()
+
     @staticmethod
-    def __handle_key_events(event: pygame.Event, handlers: defaultdict):
+    def __handle_key_events(event: pygame.event, handlers: defaultdict):
         for handler in handlers[event.key]:
             handler(event.key)
 
     @staticmethod
-    def __is_mouse_event(event: pygame.Event) -> bool:
+    def __is_mouse_event(event: pygame.event) -> bool:
         return event.type in (pygame.MOUSEBUTTONDOWN,
                               pygame.MOUSEBUTTONUP,
                               pygame.MOUSEMOTION)
